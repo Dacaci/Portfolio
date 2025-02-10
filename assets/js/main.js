a9b853c83d53d1d66c317770ea4f597c17b09c43
bilel <irumizoldik21@gmail.com>
2025-02-10 12:18:17 +0100

    Ajout des animations ScrollReveal (effets depuis la gauche et la droite)

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,5 +56,29 @@ const sr = ScrollReveal({
   
   /* -- HEADINGS -- */
   sr.reveal('.top-header', {});
+
+/* ----- SCROLL REVEAL LEFT_RIGHT ANIMATION ----- */
+
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  });
+  
+  srLeft.reveal('.about-info', { delay: 100 });
+  srLeft.reveal('.contact-info', { delay: 100 });
+  
+  /* -- ABOUT SKILLS & FORM BOX -- */
+  const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+  });
+  
+  srRight.reveal('.skills-box', { delay: 100 });
+  srRight.reveal('.form-control', { delay: 100 });
   
   
